1162fc4a76af2ba62357efb8f476336a145d83f0
AYanovDev <alex.ianovskii@mail.ru>
2026-07-09 16:43:17 +0500

    layout done

--- content/components/Contacts/Contacts.tsx
+++ content/components/Contacts/Contacts.tsx
@@ -1,13 +1,39 @@
+import "./contacts.css";
+
 export function Contacts() {
   return (
     <div>
       <h2>Contacts</h2>
       <div className="contacts">
-        <div className="phone"></div>
-        <div className="mail"></div>
-        <div className="tw"></div>
-        <div className="fb"></div>
-        <div className="skype"></div>
+        <div className="phone">
+          <img src="assets/icons.png" alt="" />
+          <p className="cont_bold">8 800 555 35 35</p>
+        </div>
+        <div className="mail">
+          <img src="assets/mail.png" alt="" />
+          <p className="cont_bold">literally@him.com</p>
+        </div>
+        <div className="tw">
+          <img src="assets/Twitter.png" alt="" />
+          <div>
+            <p className="cont_bold">Twitter</p>
+            <p className="cont_link">https://twitter.com/wordpress</p>
+          </div>
+        </div>
+        <div className="fb">
+          <img src="assets/facebook.png" alt="" />
+          <div>
+            <p className="cont_bold">Facebook</p>
+            <p className="cont_link">https://facebook.com/wordpress</p>
+          </div>
+        </div>
+        <div className="skype">
+          <img src="assets/Skype.png" alt="" />
+          <div>
+            <p className="cont_bold">Skype</p>
+            <p className="cont_link">whoeverstillusesthis.pl</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- content/components/Feedback/Feedback.tsx
+++ content/components/Feedback/Feedback.tsx
@@ -1,20 +1,37 @@
+import "./feedback.css";
+
 export function Feedback() {
   return (
     <div>
       <h2>Feedback</h2>
-      <p className="shaded_text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id rerum qui
-        nulla autem numquam itaque nam nihil explicabo neque rem maiores
-        excepturi aperiam nisi ipsa amet, blanditiis incidunt cupiditate quidem!
-      </p>
-      <div className="author"></div>
-      <p className="shaded_text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nihil
-        eligendi veniam molestiae itaque voluptatum explicabo aliquid fugit
-        quia? Provident tempore quisquam ad laborum est placeat repellendus
-        perferendis, culpa in.
-      </p>
-      <div className="author"></div>
+      <div className="feedback_container">
+        <p className="shaded_text">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+          Integer tincidunt. Cras dapibus.
+        </p>
+        <div className="author">
+          <img src="assets/comment.png" alt="" />
+        </div>
+        <p className="shaded_text">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+          nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+          Integer tincidunt. Cras dapibus.
+        </p>
+        <div className="author">
+          <img src="assets/comment.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
