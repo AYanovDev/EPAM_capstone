cae38b944d5d3e17cc4e342d9a3c000fa6929865
AYanovDev <alex.ianovskii@mail.ru>
2026-07-09 15:03:59 +0500

    skills and experience layout done

--- content/components/Experience/Experience.tsx
+++ content/components/Experience/Experience.tsx
@@ -1,9 +1,39 @@
+import "./experience.css";
+
 export function Experience() {
   return (
-    <div>
+    <div className="exp_container">
       <h2>Experience</h2>
-      <div className="exp_1"></div>
-      <div className="exp_2"></div>
+      <div className="exp_1">
+        <div className="companyANDdate">
+          <h3>Google</h3>
+          <p className="dates">2013-2014</p>
+        </div>
+        <div className="titleANDtext">
+          <h3>Front-End Developer / php programmer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quas
+            aut dolorum accusamus pariatur tenetur tempora voluptatem dolore
+            eum? Ab delectus fugit ipsa provident, odio quaerat maxime
+            perspiciatis. Cumque, nisi.
+          </p>
+        </div>
+      </div>
+      <div className="exp_2">
+        <div className="companyANDdate">
+          <h3>Twitter</h3>
+          <p className="dates">2012</p>
+        </div>
+        <div className="titleANDtext">
+          <h3>Web-Developer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quas
+            aut dolorum accusamus pariatur tenetur tempora voluptatem dolore
+            eum? Ab delectus fugit ipsa provident, odio quaerat maxime
+            perspiciatis. Cumque, nisi.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- content/components/Skills/Skills.tsx
+++ content/components/Skills/Skills.tsx
@@ -1,9 +1,30 @@
+import { Scale } from "./scale";
+import "./skills.css";
+
 export function Skills() {
   return (
     <div>
       <h2>Skills</h2>
-      <div className="chart"></div>
-      <div className="mastery"></div>
+      <div className="chart">
+        <div className="html">
+          <div>HTML</div>
+        </div>
+        <div className="css">
+          <div>CSS</div>
+        </div>
+        <div className="jquerry">
+          <div>jQuerry</div>
+        </div>
+        <div className="php">
+          <div>PHP</div>
+        </div>
+        <div className="laravel">
+          <div>Laravel 2</div>
+        </div>
+      </div>
+      <div className="mastery">
+        <Scale></Scale>
+      </div>
     </div>
   );
 }
