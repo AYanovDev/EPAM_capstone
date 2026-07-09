77f21fa4ea066973d77c227e905307be78203e09
AYanovDev <alex.ianovskii@mail.ru>
2026-07-09 16:20:27 +0500

    portfolio done

--- content/components/Contacts/Contacts.tsx
+++ content/components/Contacts/Contacts.tsx
@@ -2,7 +2,13 @@ export function Contacts() {
   return (
     <div>
       <h2>Contacts</h2>
-      <div className="contacts"></div>
+      <div className="contacts">
+        <div className="phone"></div>
+        <div className="mail"></div>
+        <div className="tw"></div>
+        <div className="fb"></div>
+        <div className="skype"></div>
+      </div>
     </div>
   );
 }
--- content/components/Portfolio/Portfolio.tsx
+++ content/components/Portfolio/Portfolio.tsx
@@ -1,9 +1,57 @@
+import "./portfolio.css";
+
 export function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
-      <div className="porfolio_nav"></div>
-      <div className="projects"></div>
+      <div className="porfolio_nav">
+        <p>All</p>
+        <p className="slash">/</p>
+        <p>Code</p>
+        <p className="slash">/</p>
+        <p>UI</p>
+      </div>
+      <div className="projects">
+        <div className="project-card">
+          <img src="assets/Portfolio cards.png" alt="" />
+          <div className="proj_text">
+            <p>Some text</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur vero omnis officiis sapiente minus nulla,
+              exercitationem aperiam at! Fugit et consequuntur non quo autem
+              porro, officiis ad cum rem delectus.
+            </p>
+            <a href="#">View resource</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src="assets/Portfolio cards.png" alt="" />
+          <div className="proj_text">
+            <p>Some text</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur vero omnis officiis sapiente minus nulla,
+              exercitationem aperiam at! Fugit et consequuntur non quo autem
+              porro, officiis ad cum rem delectus.
+            </p>
+            <a href="#">View resource</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src="assets/Portfolio cards.png" alt="" />
+          <div className="proj_text">
+            <p>Some text</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur vero omnis officiis sapiente minus nulla,
+              exercitationem aperiam at! Fugit et consequuntur non quo autem
+              porro, officiis ad cum rem delectus.
+            </p>
+            <a href="#">View resource</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
